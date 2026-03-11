9417b921db8db81921f58a4cf502b1c53a0abf63
Bikesh Kumar <iambikeshkumar07@gmail.com>
2026-03-11 17:37:27 +0530

    feat: update signup route to store Mobnumber as string; add migration to drop PendingUser table

--- startup-mela/app/api/signup/route.ts
+++ startup-mela/app/api/signup/route.ts
@@ -14,7 +14,7 @@ export async function POST(req:Request){
             name,
             email,
             password:hashed,
-            Mobnumber: parseInt(Mobnumber),
+            Mobnumber,
             otp,
             otpExpiry,
             isVerified: false
