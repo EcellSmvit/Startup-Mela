b89107d380ebf7524125315b5f0a3e1eb613b7c7
Bikesh Kumar <iambikeshkumar07@gmail.com>
2026-03-13 16:13:28 +0530

    feat: refactor login page to use Button component for Google sign-in; update signIn method to use next-auth/react

--- startup-mela/app/signup/page.tsx
+++ startup-mela/app/signup/page.tsx
@@ -1,14 +1,17 @@
-import { signIn } from "@/lib/auth";
+"use client";
+import { signIn } from "next-auth/react";
+import Button from "@/components/button";
 
 export default function LoginPage() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
-  )
+    <div className="flex flex-col gap-4">
+      {/* Existing credentials form... */}
+      
+      <Button
+        variant="secondary"
+        text="Sign in with Google"
+        onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+      />
+    </div>
+  );
 }
