aa04cb54ae3dc52858403a6512a78dd15918b8c8
Bikesh Kumar <iambikeshkumar07@gmail.com>
2026-03-13 15:50:33 +0530

    feat: implement Google sign-in functionality on login page; update authentication flow and configuration

---
+++ startup-mela/prisma/migrations/20260313101820_adding_isverfied_false/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Mobnumber` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `isVerified` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `leaderId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `otp` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `otpExpiry` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "User" DROP CONSTRAINT "User_leaderId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "Mobnumber",
+DROP COLUMN "isVerified",
+DROP COLUMN "leaderId",
+DROP COLUMN "otp",
+DROP COLUMN "otpExpiry";
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
